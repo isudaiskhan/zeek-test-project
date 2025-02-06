3bee99e18933388f1380faf0e0c4511befd00bae
Sudais Khan <134719836+isudaiskhan@users.noreply.github.com>
2025-02-06 16:24:07 +0500

    Update MediaGalleryModal page now you uploaded multiple images (#46)
    * Update MediaGalleryModal page now you uploaded multiple images
    
    * i use parallel promises for better effieciency

--- src/components/Modals/MediaGalleryModal.jsx
+++ src/components/Modals/MediaGalleryModal.jsx
@@ -1,26 +1,78 @@
-import React from "react";
+/* eslint-disable no-undef */
+import React, { useState, useCallback } from "react";
 import {
   Button,
   Dialog,
   DialogContent,
   Typography,
   IconButton,
+  Box,
 } from "@mui/material";
 import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 import CloseIcon from "@mui/icons-material/Close";
+import ClearSharpIcon from "@mui/icons-material/ClearSharp";
 import Image from "next/image";
+import { getImageBase64URL } from "@/utils/helper-functions";
 
 const MediaGalleryModal = ({ open, onClose, message, buttonText }) => {
+  const [imagePreviews, setImagePreviews] = useState([]);
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileSelection = async (e) => {
+    const files = Array.from(e.target.files);
+
+    const base64Promises = files.map((file) => getImageBase64URL(file));
+    const newPreviews = await Promise.all(base64Promises);
+
+    setSelectedFiles((prevFiles) => [...prevFiles, ...files]);
+    setImagePreviews((prevPreviews) => [...prevPreviews, ...newPreviews]);
+  };
+
+  const handleDrop = useCallback(async (e) => {
+    e.preventDefault();
+    const files = Array.from(e.dataTransfer.files);
+
+    const base64Promises = files.map((file) => getImageBase64URL(file));
+    const newPreviews = await Promise.all(base64Promises);
+
+    setSelectedFiles((prevFiles) => [...prevFiles, ...files]);
+    setImagePreviews((prevPreviews) => [...prevPreviews, ...newPreviews]);
+  }, []);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleRemove = (index) => {
+    const updatedPreviews = imagePreviews.filter((_, i) => i !== index);
+    const updatedFiles = selectedFiles.filter((_, i) => i !== index);
+
+    setImagePreviews(updatedPreviews);
+    setSelectedFiles(updatedFiles);
+  };
+
+  console.log(selectedFiles, "working");
+
   return (
     <Dialog
       open={open}
       onClose={onClose}
       fullWidth
-      maxWidth="lg"
+      maxWidth="md"
       className="backdrop-blur-sm"
       PaperProps={{
         className:
-          "rounded-lg p-20 !bg-[#00000033] !shadow-none !rounded-[20px]",
+          "rounded-lg !bg-[#00000033] !px-20 !shadow-none !rounded-[20px] !max-h-[90vh]",
+        sx: {
+          "&::-webkit-scrollbar": { width: "6px" },
+          "&::-webkit-scrollbar-track": { background: "transparent" },
+          "&::-webkit-scrollbar-thumb": {
+            background: "#888",
+            borderRadius: "10px",
+          },
+          "&::-webkit-scrollbar-thumb:hover": { background: "#555" },
+          scrollbarWidth: "thin",
+        },
       }}
     >
       <IconButton
@@ -32,31 +84,79 @@ const MediaGalleryModal = ({ open, onClose, message, buttonText }) => {
       <Typography className="!absolute !top-5 text-white !font-inter !text-2xl !left-24">
         Upload Media
       </Typography>
-      <DialogContent className="relative flex flex-col items-center border-4 rounded-[20px] cursor-pointer border-solid border-[#FFFFFF66] bg-[#00000026] !p-20">
-        <FileUploadOutlinedIcon className="!text-9xl text-[#FFFFFF66]" />
-
-        <Typography
-          variant="h6"
-          className="!mb-12 text-center !text-xl !font-inter !mt-5 text-[#FFFFFF99]"
+      <div className="py-20">
+        <DialogContent
+          className="relative flex flex-col items-center !overflow-hidden border-4 rounded-[20px] cursor-pointer border-solid border-[#FFFFFF66] bg-[#00000026] !p-8"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
         >
-          {message}
-        </Typography>
-      </DialogContent>
+          <FileUploadOutlinedIcon className="!text-9xl text-[#FFFFFF66]" />
 
-      <div className="mx-auto">
-        <Button
-          onClick={onClose}
-          className="flex items-center !bg-[#FFFFFF80] !text-black !font-inter !py-2 !px-16 !rounded-lg !text-base !mt-12 justify-center"
-        >
-          {buttonText}
-          <Image
-            src="/images/upload.svg"
-            className="!ml-5"
-            alt="upload-icon"
-            width={25}
-            height={25}
-          />
-        </Button>
+          <Typography
+            variant="h6"
+            className="!mb-12 text-center !text-xl !font-inter !mt-5 text-[#FFFFFF99]"
+          >
+            {message}
+          </Typography>
+        </DialogContent>
+
+        {imagePreviews.length > 0 && (
+          <div className="flex flex-col items-center mt-6">
+            <Box
+              className="w-full grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 overflow-y-auto max-h-[300px]"
+              sx={{
+                "&::-webkit-scrollbar": { width: "6px" },
+                "&::-webkit-scrollbar-track": { background: "transparent" },
+                "&::-webkit-scrollbar-thumb": {
+                  background: "#888",
+                  borderRadius: "10px",
+                },
+                "&::-webkit-scrollbar-thumb:hover": { background: "#555" },
+                scrollbarWidth: "thin",
+              }}
+            >
+              {imagePreviews.map((base64, index) => (
+                <div key={index} className="relative w-20 h-20 aspect-square">
+                  <Image
+                    src={base64}
+                    alt={`Image ${index + 1}`}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-md"
+                  />
+                  <IconButton
+                    onClick={() => handleRemove(index)}
+                    className="absolute -top-[10px] left-12 right-0 bg-white rounded-full"
+                  >
+                    <ClearSharpIcon className="!text-red-500" />
+                  </IconButton>
+                </div>
+              ))}
+            </Box>
+          </div>
+        )}
+        <div className="flex justify-center">
+          <Button
+            component="label"
+            className="!mt-6 !bg-[#FFFFFF80] !text-black !font-inter !py-2 !px-16 !rounded-lg !text-base "
+          >
+            {buttonText}
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              hidden
+              onChange={handleFileSelection}
+            />
+            <Image
+              src="/images/upload.svg"
+              className="!ml-5"
+              alt="upload-icon"
+              width={25}
+              height={25}
+            />
+          </Button>
+        </div>
       </div>
     </Dialog>
   );
